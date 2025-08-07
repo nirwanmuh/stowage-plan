5fd5f837db29f88ca14074bb965c1a59a4bbb5d1
nirwanmuh <59735784+nirwanmuh@users.noreply.github.com>
2025-08-07 13:07:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -167,8 +167,12 @@ def tambah_kendaraan(golongan, berat_manual=None):
                     ditempatkan = True
             else:
                 # Coba kedua orientasi hanya jika golongan ini belum dipakai
-                for size in [ukuran_asli, ukuran_asli[::-1]]:
-                    p, l = size
+                if ukuran_asli[0] >= ukuran_asli[1]:
+                    prioritas_orientasi = [ukuran_asli[::-1], ukuran_asli]
+                else:
+                    prioritas_orientasi = [ukuran_asli, ukuran_asli[::-1]]
+                
+                for size in prioritas_orientasi:                    p, l = size
                     i, j = cari_lokasi(grid, p, l, berat, tx, ty)
                     if i is not None:
                         # Simpan orientasi ini untuk golongan tersebut
